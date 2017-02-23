c3a030259f619b65e62cc14313d6251be7b0710b
Rena Tan <rena.tanrx@gmail.com>
2017-02-23 11:30:42 -0800

    purrfect match landing page, first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function(){
+	$('#change').click(function(){
+		$('#change').css("background-color", "#FFFFCC");
+	});
+	$('#color').click(function(){
+		$('#color').css("background-color", "#FFF3EA");
+	});
+});
